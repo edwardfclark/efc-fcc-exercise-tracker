12d4e92efcc011b80a8cba944e1b382e730fcf13
Edward Clark <edwardfclark@gmail.com>
2019-01-12 12:14:55 +0000

    Initial Setup
    Performed the initial setup actions - adding express and port, that sort
    of thing.

---
+++ server.js
@@ -0,0 +1,12 @@
+// Set up dependencies.
+const express = require('express');
+const app = express();
+const port = 8080;
+
+
+// Set the viewing engine.
+
+// Connect to the database.
+
+//Listen for requests.
+app.listen(port, () => console.log(`The server is listening on port ${port}.`));
